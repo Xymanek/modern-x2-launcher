6986fe7559e50aafca58bd9841e500d22209cfc2
Xymanek <xymanek@outlook.com>
2022-07-11 13:09:36 +0300

    [WIP] Config file editor

---
+++ ModernX2Launcher/ViewModels/ConfigFileEditorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ReactiveUI;
+
+namespace ModernX2Launcher.ViewModels;
+
+public class ConfigFileEditorViewModel : ViewModelBase
+{
+    private string[] _lines = Array.Empty<string>();
+
+    public string[] Lines
+    {
+        get => _lines;
+        set => this.RaiseAndSetIfChanged(ref _lines, value);
+    }
+}
--- ModernX2Launcher/ViewModels/ModInfoViewModel.cs
+++ ModernX2Launcher/ViewModels/ModInfoViewModel.cs
@@ -5,10 +5,17 @@ namespace ModernX2Launcher.ViewModels;
 public class ModInfoViewModel : ViewModelBase
 {
     private ModEntryViewModel? _modEntry;
+    private ConfigFileEditorViewModel? _activeConfigEditor = new();
 
     public ModEntryViewModel? ModEntry
     {
         get => _modEntry;
         set => this.RaiseAndSetIfChanged(ref _modEntry, value);
+    }
+
+    public ConfigFileEditorViewModel? ActiveConfigEditor
+    {
+        get => _activeConfigEditor;
+        set => this.RaiseAndSetIfChanged(ref _activeConfigEditor, value);
     }
 }
