fb63d8f1ae55f2a4e639a33d5e693dbacb8be3fa
Xymanek <xymanek@outlook.com>
2022-07-11 13:37:15 +0300

    [WIP] Config file editor - basic grammar setup

--- ModernX2Launcher/Views/ConfigFileEditorView.axaml.cs
+++ ModernX2Launcher/Views/ConfigFileEditorView.axaml.cs
@@ -1,6 +1,12 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using AvaloniaEdit;
+using AvaloniaEdit.Document;
+using AvaloniaEdit.TextMate;
+using TextMateSharp.Grammars;
 
 namespace ModernX2Launcher.Views;
 
@@ -9,10 +15,14 @@ public partial class ConfigFileEditorView : UserControl
     public ConfigFileEditorView()
     {
         InitializeComponent();
-    }
 
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
+        Editor.Background = Brushes.Transparent;
+
+        RegistryOptions registryOptions = new(ThemeName.Dark);
+        TextMate.Installation textMate = Editor.InstallTextMate(registryOptions);
+
+        textMate.SetGrammar(registryOptions.GetScopeByExtension(".ini"));
+
+        Editor.Document = new TextDocument("[X2Things]" + Environment.NewLine + "+arrStuff=123");
     }
 }
