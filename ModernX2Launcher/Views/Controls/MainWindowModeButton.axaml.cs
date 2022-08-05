0ab0b11466d9cfa7244b4e7ab5f70c1bad3b1e90
Xymanek <xymanek@outlook.com>
2022-08-05 23:20:06 +0300

    Main window overall layout reworked

---
+++ ModernX2Launcher/Views/Controls/MainWindowModeButton.axaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Material.Icons;
+
+namespace ModernX2Launcher.Views.Controls;
+
+public partial class MainWindowModeButton : UserControl
+{
+    public static readonly StyledProperty<string> ModeNameProperty =
+        AvaloniaProperty.Register<MainWindowModeButton, string>(nameof(ModeName));
+    
+    public static readonly StyledProperty<MaterialIconKind> IconProperty =
+        AvaloniaProperty.Register<MainWindowModeButton, MaterialIconKind>(nameof(Icon));
+    
+    public MainWindowModeButton()
+    {
+        InitializeComponent();
+    }
+
+    public string ModeName
+    {
+        get => GetValue(ModeNameProperty);
+        set => SetValue(ModeNameProperty, value);
+    }
+    
+    public MaterialIconKind Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}
