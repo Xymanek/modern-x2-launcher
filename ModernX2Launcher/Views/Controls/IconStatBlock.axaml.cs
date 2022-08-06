fb5db3f27af64eef61e6cef43a6015952b414303
Xymanek <xymanek@outlook.com>
2022-08-06 20:31:33 +0300

    Fixed icon stats tooltip hit test

---
+++ ModernX2Launcher/Views/Controls/IconStatBlock.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Material.Icons;
+
+namespace ModernX2Launcher.Views.Controls;
+
+public class IconStatBlock : ContentControl
+{
+    public static readonly StyledProperty<MaterialIconKind> IconProperty =
+        AvaloniaProperty.Register<IconStatBlock, MaterialIconKind>(nameof(Icon));
+    
+    public MaterialIconKind Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}
