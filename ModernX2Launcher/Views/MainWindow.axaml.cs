954e6fc48dde456538004cab2139777a58aa899f
Xymanek <xymanek@outlook.com>
2022-08-10 19:18:05 +0300

    Moved some code to XAML (that should've been there)

--- ModernX2Launcher/Views/MainWindow.axaml.cs
+++ ModernX2Launcher/Views/MainWindow.axaml.cs
@@ -1,6 +1,3 @@
-using Avalonia.Controls;
-using Avalonia.Controls.Templates;
-using Avalonia.Layout;
 using Avalonia.ReactiveUI;
 using ModernX2Launcher.ViewModels;
 
@@ -8,10 +5,6 @@ namespace ModernX2Launcher.Views;
 
 public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
-    public static readonly FuncTemplate<IPanel> ModesPanel = new(
-        () => new StackPanel { Orientation = Orientation.Vertical}
-    );
-    
     public MainWindow()
     {
         InitializeComponent();
