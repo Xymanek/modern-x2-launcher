3efb238f200550ebdb5ac78ce759ce97143b3368
Xymanek <xymanek@outlook.com>
2022-08-10 11:40:32 +0300

    Further improvements to the top bar

--- ModernX2Launcher/Views/Controls/IconStatBlock.axaml.cs
+++ ModernX2Launcher/Views/Controls/IconStatBlock.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Media;
 using Material.Icons;
 
 namespace ModernX2Launcher.Views.Controls;
@@ -10,9 +11,18 @@ public class IconStatBlock : ContentControl
     public static readonly StyledProperty<MaterialIconKind> IconProperty =
         AvaloniaProperty.Register<IconStatBlock, MaterialIconKind>(nameof(Icon));
     
+    public static readonly StyledProperty<IBrush?> IconForegroundProperty =
+            AvaloniaProperty.Register<IconStatBlock, IBrush?>(nameof(Foreground), Brushes.Black);
+    
     public MaterialIconKind Icon
     {
         get => GetValue(IconProperty);
         set => SetValue(IconProperty, value);
+    }
+    
+    public IBrush? IconForeground
+    {
+        get => GetValue(IconForegroundProperty);
+        set => SetValue(IconForegroundProperty, value);
     }
 }
