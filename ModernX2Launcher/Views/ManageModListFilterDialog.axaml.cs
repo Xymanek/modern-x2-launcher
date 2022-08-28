5a3c78cda24b62c5d3c3af383b2658ab3186f814
Xymanek <xymanek@outlook.com>
2022-08-28 22:55:08 +0300

    Some design for ManageModListFilterDialog

--- ModernX2Launcher/Views/ManageModListFilterDialog.axaml.cs
+++ ModernX2Launcher/Views/ManageModListFilterDialog.axaml.cs
@@ -10,6 +10,8 @@ public partial class ManageModListFilterDialog : Window
     public ManageModListFilterDialog()
     {
         InitializeComponent();
+        
+        AuthorSelectionListBox.Items = new object[] { "123", "456", "789" };
     }
 
     private void Window_OnClosing(object? sender, CancelEventArgs e)
